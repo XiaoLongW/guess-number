a5688ca0e8ff4c53d1f2e28fb6d5f2facb553c90
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 22:29:02 +0800

    modify answer-generator-spec.js

--- spec/answer-generator-spec.js
+++ spec/answer-generator-spec.js
@@ -3,31 +3,32 @@
 var Answer = require('../main/answer-generator.js');
 
 describe('AnswerGenerator', function() {
+
   describe('#answer()', function() {
 
-
     it('number should not repeat',function(){
-     var x=0;
-     spyOn(Math,'random').and.callFake(function(){
-         var arr = [0.1,0.1,0.2,0.3,0.4];
-         return arr[x++];
-     });
+
+      var x=0;
+      spyOn(Math, 'random').and.callFake(function () {
+        var arr = [0.1, 0.1, 0.2, 0.3, 0.4];
+        return arr[x++];
+      });
 
       var answer = new Answer(Math);
       var arr = answer.answer();
       expect(arr).toBe('1234');
-    })
+    });
 
-    it("number's length should be 4",function(){
+    it("number's length should be 4",function () {
       var answer = new Answer(Math);
       expect(answer.answer().length).toBe(4);
-    })
+    });
 
-    it('number should be random',function(){
+    it('number should be random',function () {
       var x=0;
-      spyOn(Math,'random').and.callFake(function(){
-           var arr = [0.1,0.2,0.3,0.4];
-           return arr[x++];
+      spyOn(Math, 'random').and.callFake(function(){
+        var arr = [0.1,0.2,0.3,0.4];
+        return arr[x++];
       });
 
       var answer = new Answer(Math);
@@ -36,4 +37,4 @@ describe('AnswerGenerator', function() {
     })
 
   })
-})
+});
