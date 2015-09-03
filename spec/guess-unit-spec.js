e929fe63cee82b2ded7f413c5cda76e5d7a94c8c
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 22:38:41 +0800

    modify guess-unit-spec.js

--- spec/guess-unit-spec.js
+++ spec/guess-unit-spec.js
@@ -7,19 +7,20 @@ var CompareNumber = require('../main/compare-number.js');
 describe("Guess",function(){
 
   describe('#guessRandom', function() {
-  
-      it('guessRandom can run', function() {
-        var answer =new Answer(Math);
-        spyOn(answer,'answer').and.callFake(function(){
-             return '1234';
-        });
-        var compare = new CompareNumber();
-        spyOn(compare,'game').and.callFake(function(){
-             return '0A0B';
-        });
-        var guess = new Guess(answer,compare);
-        expect(guess.guessRandom('5678')).toBe('0A0B');
+
+    it('guessRandom can run', function() {
+      var answer =new Answer(Math);
+      spyOn(answer,'answer').and.callFake(function(){
+        return '1234';
       });
 
+      var compare = new CompareNumber();
+      spyOn(compare,'game').and.callFake(function(){
+        return '0A0B';
+      });
+
+      var guess = new Guess(answer,compare);
+      expect(guess.guessRandom('5678')).toBe('0A0B');
     });
+  });
 });
