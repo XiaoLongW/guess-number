a9fc93e6c7c88afecb07975ef4010cbf72d9ee23
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 22:33:09 +0800

    modify answer-generator.js

--- main/answer-generator.js
+++ main/answer-generator.js
@@ -1,5 +1,5 @@
 'use strict';
-var _=require('lodash')
+var _=require('lodash');
 
 function AnswerGenerator(math){
   this.math = math;
@@ -10,18 +10,18 @@ AnswerGenerator.prototype.answer = function(){
   var count = 0;
 
   for(;;){
-    var a =this.math.floor(this.math.random()*10);
-    if(_.findIndex(arrNumber, function (oneArr) {return oneArr == a;}) == -1){
-       arrNumber.push(a);
-       count++;
+    var oneNumber =this.math.floor(this.math.random()*10);
+    if(_.findIndex(arrNumber, function (oneArr) {return oneArr == oneNumber;}) == -1){
+      arrNumber.push(oneNumber);
+      count++;
     }
-    if(count==4){
+    if(4 == count){
       break;
     }
   }
 
   var strNumber = ''+arrNumber[0]+arrNumber[1]+arrNumber[2]+arrNumber[3];
   return strNumber;
-}
+};
 
 module.exports = AnswerGenerator;
