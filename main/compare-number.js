f1e195eb4bd82ba34ef9b7fde48a48836f274409
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 22:35:54 +0800

    modify compare-number.js

--- main/compare-number.js
+++ main/compare-number.js
@@ -1,6 +1,5 @@
 'use strict';
-
-var _=require('lodash')
+var _=require('lodash');
 
 function CompareNumber(){
 }
@@ -9,16 +8,16 @@ CompareNumber.prototype.game=function(input,answer){
   var aCount=0;
   var bCount=0;
 
-  for(var x = 0;x<input.length;x++){
-      var indexA = _.findIndex(answer, function (oneAnswer) {return oneAnswer == input[x];});
-      if( indexA == x) {
-        aCount++;
-      }else if(indexA != -1) {
-        bCount++;
-      }
+  for(var x = 0; x<input.length; x++){
+    var indexA = _.findIndex(answer, function (oneAnswer) {return oneAnswer == input[x];});
+    if( indexA == x) {
+      aCount++;
+    }else if(indexA != -1) {
+      bCount++;
+    }
   }
 
   return aCount+'A'+bCount+'B';
-}
+};
 
 module.exports = CompareNumber;
