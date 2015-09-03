8bf34e161db23d18e4aeb917eeebe5c84dfb8502
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 22:39:10 +0800

    modify guess.js

--- main/guess.js
+++ main/guess.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var Answer = require('../main/answer-generator.js');
-var CompareNumber = require('../main/compare-number.js');
-
 function Guess(answer,compare){
   this.answer = answer;
-  this.compare = compare;;
+  this.compare = compare;
 }
 
 Guess.prototype.guessRandom = function(input){
   return this.compare.game(input, this.answer.answer());
-}
+};
 
 module.exports = Guess;
